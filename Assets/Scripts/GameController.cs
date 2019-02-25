5ef8b528c7ce391b85e833f2dac67d3e1f3cb628
Meghan Gorman <fluteloops27@gmail.com>
2019-02-25 15:01:22 -0600

    Spawning waves

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,18 +7,32 @@ public class GameController : MonoBehaviour
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnWaves();
+        StartCoroutine (SpawnWaves());
     }
 
     // Update is called once per frame
